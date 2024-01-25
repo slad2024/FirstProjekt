8c4943b796cd7e5dfb3f2967f7ce75545c5c861c
slad <t.allandji@icloud.com>
2024-01-25 21:46:44 +0100

    Initial Commit

---
+++ FirstProjekt/FirstProjektApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjektApp.swift
+//  FirstProjekt
+//
+//  Created by slad on 25.01.24.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
